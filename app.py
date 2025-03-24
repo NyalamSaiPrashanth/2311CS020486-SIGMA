c6161ef74ae9dbfe69c0ed4edf8959aacbd9afae
NyalamSaiPrashanth <sainyalam206@gmail.com>
2025-03-24 10:36:41 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,94 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "id": "f5b3f1ef-9fdd-4200-8451-e3080868a804",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      " * Serving Flask app '__main__'\n",
+      " * Debug mode: on\n"
+     ]
+    },
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.\n",
+      " * Running on http://127.0.0.1:5000\n",
+      "Press CTRL+C to quit\n",
+      " * Restarting with watchdog (windowsapi)\n"
+     ]
+    },
+    {
+     "ename": "SystemExit",
+     "evalue": "1",
+     "output_type": "error",
+     "traceback": [
+      "An exception has occurred, use %tb to see the full traceback.\n",
+      "\u001b[1;31mSystemExit\u001b[0m\u001b[1;31m:\u001b[0m 1\n"
+     ]
+    },
+    {
+     "name": "stderr",
+     "output_type": "stream",
+     "text": [
+      "C:\\Users\\sainy\\anaconda3\\Lib\\site-packages\\IPython\\core\\interactiveshell.py:3585: UserWarning: To exit: use 'exit', 'quit', or Ctrl-D.\n",
+      "  warn(\"To exit: use 'exit', 'quit', or Ctrl-D.\", stacklevel=1)\n"
+     ]
+    }
+   ],
+   "source": [
+    "from flask import Flask,request, jsonify\n",
+    "import joblib\n",
+    "import numpy as np\n",
+    "app = Flask(__name__)\n",
+    "model = joblib.load(\"model.pkl\")\n",
+    "@app.route('/')\n",
+    "def home():\n",
+    "    return\"Welcome to the ML Model API!\"\n",
+    "@app.route('/predict',methods=['POST'])\n",
+    "def predict():\n",
+    "    data = request.get_json()\n",
+    "    feature = np.array(data['features']).reshape(1,-1)\n",
+    "    prediction = model.predict(features)\n",
+    "    return jsonify({'prediction': prediction.tolist()})\n",
+    "if __name__=='__main__':\n",
+    "    app.run(debug=True)"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "97a171df-cc45-4020-94f1-a5362be7108e",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python [conda env:base] *",
+   "language": "python",
+   "name": "conda-base-py"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
